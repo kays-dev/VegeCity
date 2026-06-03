1d482cbb10615729a33d66ada4c249147baf6a96
kays-dev <krys.b@outlook.com>
2026-06-03 10:02:25 +0200

    Création du projet

---
+++ Vegecity/Vegecity/VegecityApp.swift
@@ -0,0 +1,17 @@
+//
+//  VegecityApp.swift
+//  Vegecity
+//
+//  Created by Apprenant174 on 03/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct VegecityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
