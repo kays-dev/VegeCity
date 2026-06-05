d5d19bb15dd17d3b363f4311ed0ba0a5fe278a7b
apprenant168 <apprenant168@MacBook-Pro-(2)>
2026-06-05 15:25:13 +0200

    Ajoutées structures globales

---
+++ Vegecity/Vegecity/Model.swift
@@ -0,0 +1,44 @@
+//
+//  Model.swift
+//  Vegecity
+//
+//  Created by apprenant168 on 05/06/2026.
+//
+
+import Foundation
+
+struct Plant: Identifiable, Hashable {
+    let id = UUID()
+    let nom: String
+    let type: String
+    let details : String
+    var favorites : Bool = false
+    var image: String = "photo"
+}
+
+struct Activite: Identifiable, Hashable {
+    let id = UUID()
+    let nom: String // nom du lieu
+    let description: String // description lieu : potager partagé
+    let date: Date //activité  date, heure? de l'activité
+    let image: String // image de l'activité
+    let nbrPlace: Int // nombre de place max
+    let niveau: Int //1, 2 ou 3
+    let accessibilite: Bool
+    let option: Problematique
+}
+
+struct Membre: Identifiable, Hashable {
+    let id = UUID()
+    let pseudo: String
+    let image: String
+    let description: String
+}
+
+struct Problematique: Identifiable, Hashable  {
+    let id = UUID()
+    let nom: String
+    let icone: String
+}
+
+
