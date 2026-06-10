ff8ac1617b766fd5003c393c2a6e71f80e30b230
Apprenant160 <apprenant160@macbook-pro-de-apprenant160.home>
2026-06-10 22:57:35 +0200

    add model

--- Vegecity/Vegecity/Model.swift
+++ Vegecity/Vegecity/Model.swift
@@ -28,7 +28,14 @@ struct Activite: Identifiable, Hashable {
     let accessibilite: Bool
     let inscription: Bool
     let option: Problematique
+    
+    static let activites: [Activite] = [
+        Activite(nom: "Arts et Métiers", cp: 75003, description: "Atelier d'initiation aux techniques de plantation de différente variétés de plantes", date: Date(), image: "arts_et_metiers", nbrPlace: 12, niveau: 2, accessibilite: false, inscription: false, option: Problematique.init(nom: "inondation", icone: "drop.circle")),
+        Activite(nom: "Montgallet", cp: 75012, description: "Potager partagé municipal", date: Date(), image: "montgallet", nbrPlace: 9, niveau: 1, accessibilite: true, inscription: false, option: Problematique.init(nom: "jardinage", icone: "leaf.fill"))
+    ]
 }
+
+
 
 struct Membre: Identifiable, Hashable {
     let id = UUID()
