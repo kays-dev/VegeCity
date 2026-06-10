74197f67a860a615b7465923cd216b40dd240d48
Apprenant160 <apprenant160@macbook-pro-de-apprenant160.home>
2026-06-10 22:34:49 +0200

    add new view for card

---
+++ Vegecity/Vegecity/CardListView.swift
@@ -0,0 +1,61 @@
+//
+//  CarteLieu.swift
+//  CarnetDeVoyage
+//
+//  Created by Apprenant160 on 04/06/2026.
+//
+
+import SwiftUI
+
+struct CardListView: View {
+    
+    let activite: Activite
+    
+    var body: some View {
+       
+        HStack() {
+            Image(activite.image)
+                .resizable()
+                .scaledToFill()
+                .frame(width: 100, height: 100)
+                .cornerRadius(5)
+                .padding()
+            
+            VStack(alignment: .leading, spacing: 0) {
+                Text(activite.nom)
+                    .font(.headline)
+                Text(activite.description)
+                    .font(.subheadline)
+                
+                HStack {
+                    Text("nombre places : " + activite.nbrPlace.description)
+                        .font(.caption)
+                        .foregroundStyle(Color.orange)
+                        .fontWeight(.bold)
+                        .padding(.vertical, 40)
+                        .padding(.horizontal, 0)
+                        .frame(maxWidth: .infinity,maxHeight: 100, alignment: .topLeading)
+        
+                    HStack {
+//                        Image(systemName: "figure.and.child.holdinghands")
+                        Image(systemName: "figure.roll")
+                            .font(.title2)
+//                        Image(systemName: "allergens.fill")
+                    } .padding()
+                }
+        }
+            .frame(maxWidth: .infinity,maxHeight: 100, alignment: .topLeading)
+        }
+        .border(Color.vcCardBorder)
+        .background(.background.secondary)
+        .clipShape(.rect(cornerRadius: 20))
+        .padding(16)
+        .shadow(color: .vcCardShadow, radius: 0280)
+        
+    }
+}
+
+#Preview {
+    CardListView(activite: Activite(nom: "Arts et Métiers", cp: 75003, description: "Atelier d'initiation aux techniques de plantation de différente variétés de plantes", date: Date(), image: "arts_et_metiers", nbrPlace: 12, niveau: 2, accessibilite: false, inscription: false, option: Problematique.init(nom: "inondation", icone: "drop.circle")))
+    //CarteLieuBase2()
+}
